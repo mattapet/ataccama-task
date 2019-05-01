c6bbdcfb17555438727e6efa97aa06f0fe77d2ff
Peter Matta <pet3r.matta@gmail.com>
2019-05-01 14:38:33 -0500

    Setup project

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,8 @@
+//#region imports
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+//#endregion
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
+//#region imports
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+//#endregion
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
